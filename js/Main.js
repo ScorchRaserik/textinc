ca3b5fc26fa36297482e95f51a6d2f7275e5d3ab
ScorchRaserik <backupaccount10@gmail.com>
2014-09-10 15:05:36 -0500

    Loaded borders

--- js/Main.js
+++ js/Main.js
@@ -1,19 +1,28 @@
-TXT.Main = fuction(game) {
+var TXT = {};
+
+TXT.Main = function(game) {
 
 };
 
 TXT.Main.prototype = {
 
 	preload: function() {
-	
+		this.load.image('border', 'assets/border.png');
+		this.load.image('foodButton', 'assets/foodbutton.png');
+		this.load.image('foodButton_clicked', 'assets/foodbutton_clicked.png');
 	},
 
 	create: function() {
-	
+		b1 = this.add.sprite(195, 0, 'border');
+		b1.scale.setTo(1, 60);
+		b2 = this.add.sprite(595, 0, 'border');
+		b2.scale.setTo(1, 60);
+		b3 = this.add.sprite(195, 395, 'border');
+		b3.scale.setTo(40, 1);
 	},
 	
 	update: function() {
 	
-	},
+	}
 
-}
+};
